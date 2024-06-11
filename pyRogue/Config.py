8bf45c66adf13370a651230102a4036a9a8ebdaf
Tobias Sizmann <tobias.sizmann@gmail.com>
2024-06-11 08:52:04 +0200

    adjust player accel

--- pyRogue/Config.py
+++ pyRogue/Config.py
@@ -8,4 +8,3 @@ ROGUE_PLAYER_SIZE_X         = 0.7
 ROGUE_PLAYER_SIZE_Y         = 0.7
 ROGUE_V_PLAYER_MAX          = 5  * ROGUE_PERIOD # units m/period
 ROGUE_ACC_PLAYER_MAX        = 40   * ROGUE_PERIOD**2 # units m/(period**2)
-
